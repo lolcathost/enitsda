7c3f88b6d07f5434cb3600952772bae5eeb8c9cf
David <44490090+dsecuma@users.noreply.github.com>
2018-11-29 20:09:02 +0100

    deploy de django
    Django + HTTPS + NGINX + Letsencrypt + Uwsgi

---
+++ Code/enitsda/enitsda/core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+admin.site.site_header = "ENITSDA"
+admin.site.site_title = "ENITSDA"
+admin.site.index_title = "Panel de herramientas"
+# Register your models here.
