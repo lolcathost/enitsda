5d19ef402e83721dfa34c0fcd7c54a7397125289
Jorge Peñalba <jpsminix@gmail.com>
2018-11-30 19:44:19 +0100

    https
    Añadido el listado mac dentro de https para que genere las cabezeras de todas las macs listadas antes de enviarlas.

--- Code/esp32/main/ENITSDA.c
+++ Code/esp32/main/ENITSDA.c
@@ -8,24 +8,15 @@
 #include "ENITSDA_https.h"
 #include "esp_log.h"
 
-void mac_mostrar_listados(void){
-	for(int i=0;i<mac_wifi_cuenta;i++){
-		int *p=mac_wifi_listado[i].mac;
-		printf("W -> %02x:%02x:%02x:%02x:%02x:%02x  RSSI -> %d\n",p[0],p[1],p[2],p[3],p[4],p[5],mac_wifi_listado[i].rssi);	
-	}
-	printf("# CONOCIDOS WF:[%d] BT:[%d]\n",mac_wifi_cuenta, mac_bt_cuenta);
-}
-
 int cuenta_prueba=0;
 
-void app_main(){
+void app_main(void){
     nvs_flash_init();
 	wifi_inicializar();
 	wifi_promiscuo();
-	
+
 	while(true){
 		if(canal_seleccionado > canal_maximo){
-				mac_mostrar_listados();
 				wifi_conectar_ap();
 				envio_https();
 				esp_wifi_stop();
--- Code/esp32/main/ENITSDA_https.h
+++ Code/esp32/main/ENITSDA_https.h
@@ -5,33 +5,25 @@
 extern const uint8_t server_root_cert_pem_start[] asm("_binary_server_root_cert_pem_start");
 extern const uint8_t server_root_cert_pem_end[]   asm("_binary_server_root_cert_pem_end");
 
-// Por modificar, sera POST con TOKEN y hay que meterlo con la DB de las mac wifi
-static const char *REQUEST="POST "WEB_URL" HTTP/1.1\r\n"
-    "Host: "WEB_SERVER"\r\n"
-    "User-Agent: esp-idf/1.0 esp32\r\n"
-	"Authorization: Token "TOKEN"\r\n" 
-	"Content-Type: application/json\r\n"
-	"Accept: application/json\r\n"
-	"Content-Length: 33\r\n"
-	"\r\n"
-    "{\"mac\":\"ffffffffffff\",\"nodo\":\"1\"}\r\n"	
-	"\r\n";
-
 /* Ligeramente modificada del https_request:
  - eliminados parte logs
  - restructurada funcion rtos para que tenga timeout y resetee el BIT WIFI_CONNECTED_BIT
  - quitado bucle del ejemplo
+ - añadida envio de la DB de MACs por cada sesion TLS
  */
  
 void envio_https(void){
-    char buf[512];
-    int ret, len;
-	printf("%s\n\n",REQUEST);
+	size_t written_bytes;
+	int ret;
+	char REQUEST[512];
+	char JSON[128];
+	char mac_cadena[12];
+	
 	EventBits_t uxBits;
-    uxBits=xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT, true, true, 3000/portTICK_PERIOD_MS);
+    uxBits=xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT, true, true, 10000/portTICK_PERIOD_MS);
 	if(uxBits == 0){
 		printf("No hemos podido conectar a la wifi, salimos\n");
-		goto exit_evento;	
+		return;
 	}
 	
 	esp_tls_cfg_t cfg_tls={
@@ -45,26 +37,41 @@ void envio_https(void){
 		printf("Connection established...\n");
 	}else{
 		printf("Connection failed...\n");
-		goto exit;
+		esp_tls_conn_delete(tls);    
+		return;
 	}
 	
-	size_t written_bytes = 0;
-	do{
-		ret = esp_tls_conn_write(tls, 
-								 REQUEST + written_bytes, 
-								 strlen(REQUEST) - written_bytes);
-		if (ret >= 0) {
-			written_bytes += ret;
-		} else if (ret != MBEDTLS_ERR_SSL_WANT_READ  && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
-			printf("esp_tls_conn_write  returned 0x%x\n", ret);
-			goto exit;
-		}
-	} while(written_bytes < strlen(REQUEST));
+	// Envio de MACS adaptado. Se envian todas las MACS recolectadas al servidor en una sesion TLS.
+	// Falta Bluetooth
+	for(int i=0;i<mac_wifi_cuenta;i++){
+		int *p=mac_wifi_listado[i].mac;
+		snprintf(mac_cadena,sizeof(mac_cadena),"%02x%02x%02x%02x%02x%02x",p[0],p[1],p[2],p[3],p[4],p[5]);
+		snprintf(JSON,sizeof(JSON),"{\"mac\":\"%s\",\"nodo\":\"%s\",\"tipo\":\"1\",\"rssi\":\"%d\"}\r\n",mac_cadena,ESP_NODO,mac_wifi_listado[i].rssi);
+		snprintf(REQUEST, sizeof(REQUEST), "POST %s HTTP/1.1\r\n"
+			"Host: %s\r\n"
+			"User-Agent: esp-idf/1.0 esp32\r\n"
+			"Authorization: Token %s\r\n" 
+			"Content-Type: application/json\r\n"
+			"Accept: application/json\r\n"
+			"Content-Length: %d\r\n"
+			"\r\n"
+			"%s"	
+			"\r\n",WEB_URL,WEB_SERVER,TOKEN,strlen(JSON)-2,JSON);
+			
+		written_bytes = 0;
+		printf("%s\n",REQUEST);
+/*		do{
+			ret = esp_tls_conn_write(tls, 
+			REQUEST + written_bytes, 
+			strlen(REQUEST) - written_bytes);
+			if (ret >= 0) {
+				written_bytes += ret;
+			} else if (ret != MBEDTLS_ERR_SSL_WANT_READ  && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
+				printf("esp_tls_conn_write  returned 0x%x\n", ret);
+				goto exit;
+			}
+		} while(written_bytes < strlen(REQUEST));*/
+	}
 	
-	exit:
 	esp_tls_conn_delete(tls);    
-	putchar('\n'); // JSON output doesn't have a newline at end
-	
-	exit_evento: ;
-	printf("Salimos del envio_https\n");
 }
